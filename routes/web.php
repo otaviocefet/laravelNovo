702ca023a74f00b0a3c062c0ba445238a3a56bdf
Otavio <otaviocefet@gmail.com>
2021-10-28 08:31:36 -0300

    oi

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['middleware' => 'web'], function(){
+    Route::get('/', 'HomeController@index');
+    Auth::routes();
+    Route::get('/home', 'HomeController@index')->name('home');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::get('/usuarios', 'UsuariosController@index')->middleware('auth');
+Route::get('/usuarios/new', 'UsuariosController@new')->middleware('auth');
+Route::post('usuarios/add', 'UsuariosController@add')->middleware('auth');
+Route::get('usuarios/{id}/edit', 'UsuariosController@edit')->middleware('auth');
+Route::post('/usuarios/update/{id}', 'UsuariosController@update')->middleware('auth');
+Route::delete('/usuarios/delete/{id}', 'UsuariosController@delete')->middleware('auth');
