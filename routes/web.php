ffbc6b60f7418376e437a58ba89332f2ea16ac62
Otavio <otaviocefet@gmail.com>
2021-11-03 12:11:47 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::group(['middleware' => 'web'], function(){
-    Route::get('/', 'HomeController@index');
-    Auth::routes();
-    Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/usuarios', 'UsuariosController@index')->middleware('auth');
-Route::get('/usuarios/new', 'UsuariosController@new')->middleware('auth');
-Route::post('usuarios/add', 'UsuariosController@add')->middleware('auth');
-Route::get('usuarios/{id}/edit', 'UsuariosController@edit')->middleware('auth');
-Route::post('/usuarios/update/{id}', 'UsuariosController@update')->middleware('auth');
-Route::delete('/usuarios/delete/{id}', 'UsuariosController@delete')->middleware('auth');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
